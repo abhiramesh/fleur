0c5046e9612b9a6063722410bd45a1a65588165b
Abhi Ramesh <abhiramesh@gmail.com>
2013-10-27 23:27:20 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fleur::Application.config.secret_token = 'c5d58856b62729ff1a7a9747862f9e0b093fb261eb899d68f7a571cd0e85bb6f968699fe45bdc03a3be123935bccbec6e27cedf29aedb57f9279eee805278111'
