ce7b3abd4a9e80808089bd555c1a765291b77e31
Abhi Ramesh <abhiramesh@gmail.com>
2013-11-14 16:23:39 -0500

    fix if graph bug in user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,20 +29,24 @@ class Api::V1::UsersController < ApplicationController
 				render json: {auth_token: user.authentication_token, email: user.email, sign_in_count: user.sign_in_count.to_s, image: user.image}
 			elsif !user && !auth
 				graph = Koala::Facebook::API.new(params["oauth_token"])
-				me = graph.get_object("me")
-				uid = me["id"]
-				name = me["name"]
-				email = me["email"]
-				location = me["location"]["name"]
-				new_user = User.create(email: email, password: Devise.friendly_token, location: location)
-				if new_user.save
-					new_user.delay.get_profile_image
-					new_user.delay.send_user_to_predict
-					auth = Authorization.create(user_id: new_user.id, provider: "facebook", oauth_token: params["oauth_token"], uid: uid, name: name)
-					sign_in new_user
-					render json: {auth_token: new_user.authentication_token, email: new_user.email, sign_in_count: new_user.sign_in_count.to_s, image: new_user.image}
+				if graph
+					me = graph.get_object("me")
+					uid = me["id"]
+					name = me["name"]
+					email = me["email"]
+					location = me["location"]["name"]
+					new_user = User.create(email: email, password: Devise.friendly_token, location: location)
+					if new_user.save
+						new_user.delay.get_profile_image
+						new_user.delay.send_user_to_predict
+						auth = Authorization.create(user_id: new_user.id, provider: "facebook", oauth_token: params["oauth_token"], uid: uid, name: name)
+						sign_in new_user
+						render json: {auth_token: new_user.authentication_token, email: new_user.email, sign_in_count: new_user.sign_in_count.to_s, image: new_user.image}
+					else
+						render :json => {:status => new_user.errors.full_messages}
+					end
 				else
-					render :json => {:status => new_user.errors.full_messages}
+					render json: {status: ["Something went wrong!"]}					
 				end
 			elsif user && !auth
 				render json: {status: ["An account with that email already exists! Please login."]}
