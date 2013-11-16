417b97b2cc936b8fb634cf5dbddc17be88309ca4
Abhi Ramesh <abhiramesh@gmail.com>
2013-11-15 19:08:35 -0500

    fix

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -7,14 +7,18 @@ class Api::V1::VotesController < ApplicationController
 		item = Item.where(id: params["item_id"].to_i).first
 		if item && current_user
 			vote = Vote.where(item_id: item.id, user_id: current_user.id).first
-			vote.like = true
-			vote.save!
-			action = Action.create(:title => "swipe", :points => 1, :user_id => current_user.id)
-			current_user.points = current_user.points.to_i + action.points
-			current_user.save
-			if vote.save && action.save && current_user.save
-				vote.delay.send_like_to_predict
-				render json: { status: "Item Liked!" }
+			if vote
+				vote.like = true
+				vote.save!
+				action = Action.create(:title => "swipe", :points => 1, :user_id => current_user.id)
+				current_user.points = current_user.points.to_i + action.points
+				current_user.save
+				if vote.save && action.save && current_user.save
+					vote.delay.send_like_to_predict
+					render json: { status: "Item Liked!" }
+				else
+					render json: { status: "Something went wrong!" }
+				end
 			else
 				render json: { status: "Something went wrong!" }
 			end
@@ -27,14 +31,18 @@ class Api::V1::VotesController < ApplicationController
 		item = Item.where(id: params["item_id"].to_i).first
 		if item && current_user
 			vote = Vote.where(item_id: item.id, user_id: current_user.id).first
-			vote.like = false
-			vote.save!
-			action = Action.create(:title => "swipe", :points => 1, :user_id => current_user.id)
-			current_user.points = current_user.points.to_i + action.points
-			current_user.save
-			if vote.save && action.save && current_user.save
-				vote.delay.send_dislike_to_predict
-				render json: { status: "Item Disliked!" }
+			if vote
+				vote.like = false
+				vote.save!
+				action = Action.create(:title => "swipe", :points => 1, :user_id => current_user.id)
+				current_user.points = current_user.points.to_i + action.points
+				current_user.save
+				if vote.save && action.save && current_user.save
+					vote.delay.send_dislike_to_predict
+					render json: { status: "Item Disliked!" }
+				else
+					render json: { status: "Something went wrong!" }
+				end
 			else
 				render json: { status: "Something went wrong!" }
 			end
@@ -47,15 +55,19 @@ class Api::V1::VotesController < ApplicationController
 		item = Item.where(id: params["item_id"].to_i).first
 		if item && current_user
 			vote = Vote.where(item_id: item.id, user_id: current_user.id).first
-			vote.like = true
-			vote.love = true
-			vote.save!
-			action = Action.create(:title => "love", :points => 1, :user_id => current_user.id)
-			current_user.points = current_user.points.to_i + action.points
-			current_user.save
-			if vote.save && action.save && current_user.save
-				vote.delay.send_love_to_predict
-				render json: { status: "Item Loved!" }
+			if vote
+				vote.like = true
+				vote.love = true
+				vote.save!
+				action = Action.create(:title => "love", :points => 1, :user_id => current_user.id)
+				current_user.points = current_user.points.to_i + action.points
+				current_user.save
+				if vote.save && action.save && current_user.save
+					vote.delay.send_love_to_predict
+					render json: { status: "Item Loved!" }
+				else
+					render json: { status: "Something went wrong!" }
+				end
 			else
 				render json: { status: "Something went wrong!" }
 			end
